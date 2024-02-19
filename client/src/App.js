5d6b3732a58d6e03fe4ba5f61fdf6f73504879f5
HemantDutta <hemantnorddutta2@gmail.com>
2024-02-19 23:16:50 +0530

    Add Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/about"} element={<Home/>}/>
+                    <Route path={"/contact"} element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,31 @@
+import {Link} from "react-router-dom";
+
 export const Navbar = ({page}) => {
     return(
         <>
-            <nav></nav>
+            <nav>
+                <div className="nav-container flex justify-between">
+                    <div className="left">
+                        <ul>
+                            <li>
+                                <img src="/assets/navbar/en_ham.png" alt="Menu"/>
+                            </li>
+                            <li>
+                                <Link to={"/"} className={`${page==="home"?"active":""}`}>Home</Link>
+                            </li>
+                            <li>
+                                <Link to={"/"} className={`${page==="about"?"active":""}`}>About</Link>
+                            </li>
+                            <li>
+                                <Link to={"/"} className={`${page==="docs"?"active":""}`}>Docs</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <Link to={"/contact"}>Contact Me</Link>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
