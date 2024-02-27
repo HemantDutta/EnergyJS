38da96dce279b8115f6aa329912cdcf6ae550ed1
HemantDutta <hemantnorddutta2@gmail.com>
2024-02-27 16:04:20 +0530

    Create Hero Design #1

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,20 +8,20 @@ export const Navbar = ({page}) => {
                     <div className="left">
                         <ul className="flex items-stretch">
                             <li className="group duration-500 py-8 sm:px-8 px-1 grid place-items-center cursor-pointer hover:bg-white hover:text-black">
-                                <img src="/assets/navbar/en_ham.png" className="sm:w-1/2 w-1/4 group-hover:invert" alt="Menu"/>
+                                <img src="/assets/navbar/en_ham.png" className="sm:w-1/4 w-1/4 group-hover:invert" alt="Menu"/>
                             </li>
                             <li className="lg:grid hidden place-items-center">
-                                <Link className={`${page==="home"?"active":""} duration-500 h-full grid place-items-center text-3xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>Home</Link>
+                                <Link className={`${page==="home"?"active":""} duration-500 h-full grid place-items-center text-2xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>Home</Link>
                             </li>
                             <li className="lg:grid hidden place-items-center">
-                                <Link className={`${page==="about"?"active":""} duration-500 h-full grid place-items-center text-3xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>About</Link>
+                                <Link className={`${page==="about"?"active":""} duration-500 h-full grid place-items-center text-2xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>About</Link>
                             </li>
                             <li className="lg:grid hidden place-items-center">
-                                <Link className={`${page==="docs"?"active":""} duration-500 h-full grid place-items-center text-3xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>Docs</Link>
+                                <Link className={`${page==="docs"?"active":""} duration-500 h-full grid place-items-center text-2xl font-bold text-white py-8 px-8 cursor-pointer hover:bg-white hover:text-black`} to={"/"}>Docs</Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="right duration-500 text-3xl font-bold text-white py-8 sm:grid hidden px-8 cursor-pointer hover:bg-white hover:text-black place-items-center">
+                    <div className="right duration-500 text-2xl font-bold text-white py-8 sm:grid hidden px-10 cursor-pointer hover:bg-white hover:text-black place-items-center">
                         <Link className="h-full w-full grid place-items-center" to={"/contact"}>Contact Me</Link>
                     </div>
                 </div>
