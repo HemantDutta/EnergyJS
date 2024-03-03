73732e07c6f377cbe6a75339a6804f9f6fcbd0fe
HemantDutta <hemantnorddutta2@gmail.com>
2024-03-03 14:00:27 +0530

    Complete Documentation

--- client/src/pages/Docs.js
+++ client/src/pages/Docs.js
@@ -6,261 +6,24 @@ import {SubDocTab} from "../components/SubDocTab";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import {atomOneDarkReasonable} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {useLocation} from "react-router-dom";
+import {blurMethods, chainMethods, jsxReactClick, jsxReactLoad, opacityMethods, jsxReactString, scaleMethods, rotateMethods, translateMethods, selectorGuideline, vanillaHTMLClick, vanillaHTMLLoad, vanillaHTMLString, brightnessMethods} from "../static/static";
 
 export const Docs = () => {
 
     //Get State Location
-    const location = useLocation().state || "";
-
-    //Static Code String
-    const vanillaHTMLString = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Web Page</title>
-</head>
-<body>
-    <!-- Your HTML content -->
-    
-<!-- Link EnergyJS library -->
-<script src="path/to/energy.min.js"></script>
-<!-- Link your main JavaScript file -->
-<script src="path/to/main.js"></script>
-</body>
-</html>
-`;
-    const jsxReactString = `// Inside your React component file
-import React from 'react';
-import {Energy} from './energy.js';
-
-function YourComponent(){
-    //You component logic here
-}
-
-export default YourComponent;
-`;
-
-    const jsxReactClick = `// Inside your React component file
-import React from 'react';
-import {Energy} from './energy.js';
-
-function YourComponent(){
-
-    function animateOnClick(){
-        const box = new Energy("#box");
-        box.translate2D(-100, 100, 800);
-    }
-    
-    return(
-        <>
-            <div className="box" id="box"></div>
-            <button type="button" onClick={animateOnClick}>Click me</button>  
-        </>
-    )
-}
-
-export default YourComponent;
-`;
-
-    const jsxReactLoad = `// Inside your React component file
-import React from 'react';
-import {Energy} from './energy.js';
-
-function YourComponent(){
-
-    useEffect(()=>{
-        const box = new Energy("#box");
-        box.translate2D(-100, 100, 800);
-    },[])
-    
-    return(
-        <>
-            <div className="box" id="box"></div>
-        </>
-    )
-}
-
-export default YourComponent;
-`;
-
-    const selectorGuideline = `// How to pass selectors in the constructor
-    
-        //ID based selector
-        const box = new Energy("#box");
-        
-        //Class based selector
-        const box = new Energy(".box");
-        
-        //Tag Based Selector
-        const box = new Energy("div");
-`;
-
-    const chainMethods = `// How to chain multiple animation methods
-        const box = new Energy("#box");
-        box.translate2D(-100, 100, 800);
-        box.blur(100, 800);
-`;
-
-    const translateMethods = `// Translate Methods
-    
-        //Create Energy Object
-        const box = new Energy("#box");
-        
-        //Translate From both X and Y axis Offsets
-        box.translate2D(-100, 100, 800);
-        
-        //Translate To both X and Y axis Offsets
-        box.translate2DTo(-100, 100, 800);
-        
-        //Translate Horizontally from an X offset
-        box.translateXOffset(100, 800);
-        
-        //Translate Horizontally To an X offset
-        box.translateXTo(100, 800);
-        
-        //Translate Vertically from an Y offset
-        box.translateYOffset(100, 800);
-        
-        //Translate Vertically To an Y offset
-        box.translateYTo(100, 800);
-        
-`;
-
-    const scaleMethods = `// Scale Methods
-    
-        //Create Energy Object
-        const box = new Energy("#box");
-        
-        //Scale From both X and Y axis Values
-        box.scale2D(100, 100, 800);
-        
-        //Scale To both X and Y axis Values
-        box.scale2DTo(100, 100, 800);
-        
-        //Scale Horizontally from an X value
-        box.scaleX(100, 800);
-        
-        //Scale Horizontally To an X value
-        box.scaleXTo(100, 800);
-        
-        //Scale Vertically from an Y Value
-        box.scaleY(100, 800);
-        
-        //Scale Vertically To an Y value
-        box.scaleYTo(100, 800);
-        
-`;
-
-    const rotateMethods = `// Rotate Methods
-    
-        //Create Energy Object
-        const box = new Energy("#box");
-        
-        //Rotation from a degree value
-        box.rotate(-360, 800);
-        
-        //Rotate to a degree value
-        box.rotateTo(45, 800);
-        
-        //Rotate on a specific axis
-        box.rotateAxis("x", 45, 800);  
-        
-        //Rotate To on a specific axis
-        box.rotateAxisTo("y", -45, 800);  
-        
-`;
-
-    const opacityMethods = `// Opacity Methods
-    
-        //Create Energy Object
-        const box = new Energy("#box");
-        
-        //Opacity from a certain amount to 1
-        box.opacity(0.5, 800);
-        
-        //Opacity from a 1 to a certain amount
-        box.opacityRev(0.5, 800);
-        
-        //Opacity set to certain amount
-        box.opacityTo(0.5, 800);
-`;
-
-    const blurMethods = `// Blur Methods
-    
-        //Create Energy Object
-        const box = new Energy("#box");
-        
-        //Blur from a certain pixel value to 0
-        box.blur(100, 800);
-        
-        //Blur to a certain pixel value from 0
-        box.blurTo(100, 800);
-`;
-
-    const vanillaHTMLClick = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Web Page</title>
-</head>
-<body>
-    <div class="box" id="box"></div>
-    <button type="button" onclick="animateBox()">Click Me</button>
-
-<script src="path/to/energy.min.js"></script>
-<script> 
-function animateBox() {
-    const box = new Energy("#box");
-    box.translate2D(-100, 100, 800);
-}
-</script>
-</body>
-</html>
-`;
-
-    const vanillaHTMLLoad = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Web Page</title>
-</head>
-<body>
-    <div class="box" id="box"></div>
-    
-<script src="path/to/energy.min.js"></script>
-<script> 
-
-// First way: Using Global Scope
-const box = new Energy("#box");
-box.translate2D(-100, 100, 800);
-
-// Second way: Add an event listener on the html document
-document.addEventListener("load", animateBox);
-
-function animateBox() {
-    const box = new Energy("#box");
-    box.translate2D(-100, 100, 800);
-}
-</script>
-</body>
-</html>
-`;
+    const location = useLocation().state || "getStarted";
 
     //States
     const [activeComp, setActiveComp] = useState("getStarted");
 
     //Set Location State if any
-    useEffect(()=>{
-        if(first.current) {
+    useEffect(() => {
+        if (first.current) {
             first.current = false;
-        }
-        else {
+        } else {
             setActiveComp(location);
         }
-    },[])
+    }, [])
 
     //Refs
     const first = useRef(true);
@@ -573,6 +336,22 @@ function animateBox() {
                                             <SubDocTab head="Available Methods" tag="Here are all the Blur based methods and their implementation: ">
                                                 <SyntaxHighlighter language="html" style={atomOneDarkReasonable}>
                                                     {blurMethods}
+                                                </SyntaxHighlighter>
+                                            </SubDocTab>
+                                        </DocTab>
+                                    </>
+                                }
+                                {
+                                    activeComp === "brightness" &&
+                                    <>
+                                        <DocTab head="Brightness" tag="Here's how to use all the Brightness animation methods">
+                                            <ul className="trans-list list-disc pl-10">
+                                                <li className="text-white">"amt" argument expects a pixel value. e.g. <span className="blue-2 kode">box.blur(amt:100,dur:800)</span></li>
+                                                <li className="text-white">"dur" argument expects duration in milliseconds.</li>
+                                            </ul>
+                                            <SubDocTab head="Available Methods" tag="Here are all the Brightness based methods and their implementation: ">
+                                                <SyntaxHighlighter language="html" style={atomOneDarkReasonable}>
+                                                    {brightnessMethods}
                                                 </SyntaxHighlighter>
                                             </SubDocTab>
                                         </DocTab>
