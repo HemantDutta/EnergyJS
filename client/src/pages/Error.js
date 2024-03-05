1a8b08d541d54a3b28c6c4da838018c7c35e95c8
HemantDutta <hemantnorddutta2@gmail.com>
2024-03-05 11:35:49 +0530

    Fix BrowserRouter Render Fail Issue

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -19,17 +19,19 @@ export const Error = () => {
     }, [])
 
     //Clear Interval
-    if (count < 1) {
-        nav("/");
-    }
+    useEffect(() => {
+        if (count < 1) {
+            nav("/");
+        }
+    }, [count])
 
     return (
         <>
             <div className="error h-screen w-screen flex items-center flex-col justify-center gap-5 relative" id="error">
-                <div className="overlay absolute top-0 left-0 h-screen w-screen z-20 bg-black opacity-80"/>
+                <div className="overlay absolute top-0 left-0 h-screen w-screen z-20 bg-black opacity-90"/>
                 <img src="/assets/404/en_404_2.jpg" alt="404 Page" className="absolute top-0 left-0 object-center object-cover h-screen w-screen z-10"/>
-                <span className="main text-gradient text-9xl z-50 kode">404</span>
-                <span className="tag text-white text-4xl kode z-50">Page not found</span>
+                <span className="main text-gradient text-5xl sm:text-9xl z-50 kode">404</span>
+                <span className="tag text-white text-2xl sm:text-4xl kode z-50">Page not found</span>
                 <span className="reroute text-white z-50">Rerouting in {count} seconds...</span>
             </div>
         </>
